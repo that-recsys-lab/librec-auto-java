3cf0a6f3e3a02513ce49efa7806c8560ad90e882
Mansoury <m.mansoury@tue.nl>
2020-01-19 23:22:11 +0100

    librec 2.0.0 code

--- auto/src/main/java/net/that_recsys_lab/auto/cmd/ExpCmd.java
+++ auto/src/main/java/net/that_recsys_lab/auto/cmd/ExpCmd.java
@@ -1,7 +1,5 @@
 package net.that_recsys_lab.auto.cmd;
 
-import net.librec.data.DataModel;
-import net.librec.recommender.item.RecommendedItem;
 import net.that_recsys_lab.auto.AutoRecommenderJob;
 import net.that_recsys_lab.auto.IJobCmd;
 import net.librec.common.LibrecException;
@@ -11,8 +9,6 @@ import net.librec.recommender.RecommenderContext;
 import net.librec.util.ReflectionUtil;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
 
 public class ExpCmd implements IJobCmd{
     private AutoRecommenderJob job;
@@ -40,17 +36,17 @@ public class ExpCmd implements IJobCmd{
      * @throws LibrecException
      */
     public void execute() throws IOException, ClassNotFoundException, LibrecException {
-        job.getLOG().info("ExpCMD: START - Executing recommender job on "+String.valueOf(this.m_splitId)+".");
+        job.getLOG().info("ExpCMD: START - Executing recommender job on "+String.valueOf(this.m_splitId+1)+".");
         getConf().set("data.model.splitter", "testset");
         Integer dataDir = getConf().get("dfs.data.dir").length()+1;  // used to remove the extra "data/" segment of the path...
         getConf().set("data.splitter.cv.index", String.valueOf(this.m_splitId));
         getConf().set("data.input.path", trainFileNameGen().substring(dataDir));
         getConf().set("data.testset.path", testFileNameGen().substring(dataDir));
-//        try {
-//            job.getData().buildDataModel();
-//        } catch (LibrecException e) {
-//            e.printStackTrace();
-//        }
+        try {
+            job.getData().buildDataModel();
+        } catch (LibrecException e) {
+            e.printStackTrace();
+        }
         executeRecommenderJob();
         job.getLOG().info("ExpCMD: COMPLETE");
     }
@@ -70,59 +66,11 @@ public class ExpCmd implements IJobCmd{
      */
 
     private void executeRecommenderJob() throws ClassNotFoundException, LibrecException, IOException {
-//        job.m_data = ReflectionUtil.newInstance((Class<DataModel>) job.getDataModelClass(), job.getConf());
-//        job.getData().buildDataModel();
         RecommenderContext context = new RecommenderContext(getConf(), job.getData());
+        job.generateSimilarityAutoOverload(context);
         Recommender recommender = (Recommender) ReflectionUtil.newInstance((Class<Recommender>) job.getRecommenderClass(), getConf());
-//        job.m_cvEvalResults = new HashMap<>();
-
-        if(job.m_data.getDataSplitter().getTrainData() == null)
-            job.getLOG().info("1. train data is null");
-        if(job.m_data.getDataSplitter().getTrainData() == null)
-            job.m_data.hasNextFold();
-        job.m_data.nextFold();
-        context.setDataModel(job.getData());
-        if(job.m_data.getDataSplitter().getTrainData() == null)
-            job.getLOG().info("2. train data is null");
-
-        //Should be removed
-        job.SaveSplittedData(job.m_data.getDataSplitter().getTrainData(), this.m_splitId, "train");
-        job.SaveSplittedData(job.m_data.getDataSplitter().getTestData(), this.m_splitId, "test");
-
-//        if(job.m_data.hasNextFold()) {
-//            job.m_data.nextFold();
-//            context.setDataModel(job.m_data);
-            job.generateSimilarityAutoOverload(context);
-            recommender.train(context);
-            job.m_recommenders.add(this.m_splitId-1, recommender);
-//            job.executeEvaluatorAutoOverload(recommender, context);
-//            getConf().set("data.splitter.cv.index", String.valueOf(this.m_splitId));
-//            boolean isRanking = getConf().getBoolean("rec.recommender.isranking");
-//            List<RecommendedItem> recommendedList = null;
-//            if (isRanking){
-//                recommendedList = recommender.getRecommendedList(recommender.recommendRank());
-//            } else {
-//                recommendedList = recommender.getRecommendedList(recommender.recommendRating(context.getDataModel().getTestDataSet()));
-//            }
-//            recommendedList = filterResult(recommendedList);
-//            saveResult(recommendedList);
-//            index++;
-//        }
-
-
-//        context.setDataModel(job.m_data);
-//        job.generateSimilarityAutoOverload(context);
-//
-//
-//        boolean isRanking = getConf().getBoolean("rec.recommender.isranking");
-//        if (isRanking){
-//            recommender.getRecommendedList(recommender.recommendRank());
-//        } else {
-//            recommender.getRecommendedList(recommender.recommendRating(context.getDataModel().getTestDataSet()));
-//        }
-//
-//        recommender.recommend(context);
-//        job.m_recommenders.add(this.m_splitId-1, recommender);
+        recommender.recommend(context);
+        job.m_recommenders.add(this.m_splitId-1, recommender);
     }
 
     /**                              ~  Auto Aux functions  ~
